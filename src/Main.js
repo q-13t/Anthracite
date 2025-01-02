d6b4ab3cdb38ef3c1424c191e82bcddc6c44e591
q-13t <vova29092002@gmail.com>
2025-01-02 11:43:28 +0200

    init

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+import React from 'react';
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                
+            </div>
+        );
+    }
+}
