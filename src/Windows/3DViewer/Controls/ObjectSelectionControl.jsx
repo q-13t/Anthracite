26a3e1f1251ae5e62e2433e3eef08071f4314134
q-13t <vova29092002@gmail.com>
2025-02-09 12:27:03 +0200

    IMP: Object Removal

--- src/Windows/3DViewer/Controls/ObjectSelectionControl.jsx
+++ src/Windows/3DViewer/Controls/ObjectSelectionControl.jsx
@@ -1,6 +1,20 @@
+import TrashCan from "../../../assets/TrashCan.svg";
 const ObjectSelectionControl = ({ object, selectObject, selectedObject }) => {
+
+    const handleDeleteSelf = () => {
+        if (object.parent.children.length === 1) {
+            object.parent.parent.remove(object.parent);
+        } else {
+            object.parent.remove(object);
+        }
+        if (selectedObject === object) selectObject(null);
+    }
+
     return (
-        <p onClick={() => { selectObject(object) }} className={selectedObject === object ? "Selected-Object" : "Selectable-Object"}>{object.name ? object.name : object.type}</p>
+        <div onClick={() => { selectObject(object) }} className={selectedObject === object ? "Selectable-Object-Container Selected-Object" : "Selectable-Object-Container Selectable-Object"}>
+            <p >{object.name ? object.name : object.type}</p>
+            <img src={TrashCan} onClick={handleDeleteSelf} className="Self-Delete-Object" />
+        </div>
     );
 }
 
